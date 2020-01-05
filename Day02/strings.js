6d96b68158b39b3b1fe30996964a2a233d233000
TizVic <tiziano.vicentini@tiscali.it>
2020-01-05 14:39:32 +0100

    added extra exercices day 2

--- Day02/strings.js
+++ Day02/strings.js
@@ -127,6 +127,54 @@ console.log(quote);
 const mtQuote = '"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead."';
 console.log(mtQuote);
 
+// Added exercises
+
+// 33. Check if 'on' is found in both python and jargon
+console.log('Check if \'on\' is found in both python and jargon:', 'python'.includes('on') && 'jargon'.includes('on'));
+
+// 34. I hope this course is not full of jargon. Check if jargon is in the sentence.
+console.log('I hope this course is not full of jargon. Check if jargon is in the sentence:', 'I hope this course is not full of jargon.'.includes('jargon'))
+
+// 35. Generate a random number between 0 and 100 inclusive.
+console.log('Generate a random number between 0 and 100 inclusive:', Math.floor(Math.random() * 101));
+
+// 36. Generate a random number between 50 and 100 inclusive.
+console.log('Generate a random number between 50 and 100 inclusive:', Math.floor(50 + Math.random() * 51));
+
+// 37. Generate a random number between 0 and 255 inclusive.
+console.log('Generate a random number between 0 and 255 inclusive:', Math.floor(Math.random() * 256));
+
+// 38. Access the 'JavaScript' string characters using a random number.
+let js = 'JavaScript';
+console.log('Access the \'JavaScript\' string characters using a random number:', 
+    js[Math.floor(Math.random() * (js.length))]);
+
+// 39. Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
+let longString =
+'1 1 1 1 1\n\
+2 1 2 4 8\n\
+3 1 3 9 27\n\
+4 1 4 16 64\n\
+5 1 5 25 125';
+console.log(longString);
+
+// 40. Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+console.log('Check if typeof \'10\' is exactly equal to 10. If not make it exactly equal.',
+    '\ntypeof \'10\'', typeof '10', 
+    '\ntypeof 10', typeof 10, 
+    '\ntypeof parseInt(\'10\')', typeof parseInt('10'));
+
+// 41. Check if parseInt('9.8') is equal to 10 if not make it exactly equal with 10.
+console.log('Check if parseInt(\'9.8\') is equal to 10 if not make it exactly equal with 10.',
+    '\nparseInt(\'9.8\')', parseInt('9.8'),
+    '\nmake it exactly equal with 10:', parseInt('9.8') + 1);
+
+
 
 
 
