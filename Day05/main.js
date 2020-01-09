a2637e40a6e0114b6e7b93d604c7ffb44844277c
Tiziano Vicentini <tizvic@mc036.local>
2020-01-09 22:49:50 +0100

    Day 5 final

--- Day05/main.js
+++ Day05/main.js
@@ -136,5 +136,73 @@ if (isAllergicToHoney) {
 }
 shoppingCart[shoppingCart.indexOf('Tea')] = 'Green Tea';
 
+// 27. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'.
+//     If it does not exist add to the countries list.
+if (countries.includes('Ethiopia')) console.log(countries[countries.indexOf('Ethiopia')].toUpperCase());
+
+// 28. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'.
+//     If it does not exist add Sass to the array and print the array.
+if (webTechs.includes('Saas')) {
+    console.log('Sass is a CSS preprocess');
+} else {
+    webTechs.push('Saas');
+    console.log(webTechs);
+}
+
+// 29. Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node','Express', 'MongoDB'];
+let fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+// 30. The following is an array of 10 students ages:
+//     - Sort the array and find the min and max age
+//     - Find the min age and the max age
+//     - Find the median age(one middle item or two middle items divided by two)
+//     - Find the average age(all items divided by number of items)
+//     - Find the range of the ages(max minus min)
+//     - Compare the value of (min - average) and (max - average), use abs() method 1. 
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+ages.sort();
+console.log(`min:${ages[0]}, max:${ages[ages.length - 1]}`);
+
+let medianIndex = ages.length / 2;
+if (medianIndex % 1 !== 0) {
+    console.log(`median:${ages[medianIndex - 0.5]}`);
+} else {
+    console.log(`median (central elements):${(ages[medianIndex - 1] + ages[medianIndex])/2}`);
+}
+let sum = 0;
+ages.forEach((element) => { sum += element });
+let avg = sum / ages.length;
+console.log('average:', avg);
+
+console.log(`range: ${ages[ages.length - 1] - ages[0]}`);
+
+console.log(`min - average:${Math.abs(ages[0] - avg)}, max - average:${ages[ages.length - 1] - avg}`);
+
+// 31. Slice the first ten countries from the countries array
+console.log(countries.slice(10));
+
+// 32. Find the middle country(ies) in the countries array
+medianIndex = countries.length / 2;
+if (medianIndex % 1 !== 0) {
+    console.log(`central country:${countries[medianIndex - 0.5]}`);
+} else {
+    console.log('central countries:',countries[medianIndex - 1], countries[medianIndex]);
+}
+
+// 33. Divide the countries array into two equal arrays if it is even. 
+//     If countries array is not even , one more country for the first half.
+let firstHalf, secondHalf;
+if (medianIndex % 1 !== 0) {
+    firstHalf = countries.slice(0, medianIndex + 0.5);
+    secondHalf = countries.slice(medianIndex + 0.5);
+    console.log(firstHalf, secondHalf);
+} else {
+    firstHalf = countries.slice(0, medianIndex);
+    secondHalf = countries.slice(medianIndex);
+    console.log(firstHalf, secondHalf);
+}
 
 
