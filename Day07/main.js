7ddad0e613ace506c32109e77496167e14259ebe
TizVic <tiziano.vicentini@tiscali.it>
2020-01-12 12:52:25 +0100

    Level 2 partial

--- Day07/main.js
+++ Day07/main.js
@@ -139,5 +139,93 @@ console.log('a=1, b=2, c=3, x=0', solveLinEquation(1,2,3,0));
 // L2.02 Quadratic equation is calculated as follows: ax2 + bx + c = 0. 
 //       Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
 const solveQuadEquation = (a, b, c) => {
-    
+    if (a == 0 || a === undefined) return 'Not a quadratic equation!';
+    let B = b / a;
+    let C = c / a;
+    let delta = (B / 2) ** 2 - C;
+    let solutions = [0, 0];
+    if (delta < 0) {
+        return 'Quadratic equation with solution in complex plane';
+    } else {
+        solutions[0] = B /2 - Math.sqrt(delta);
+        solutions[1] = B /2 + Math.sqrt(delta);
+    }
+    return solutions;
 }
+
+console.log(solveQuadEquation());
+console.log(solveQuadEquation(1, 4, 4)); 
+console.log(solveQuadEquation(1, -1, -2)); 
+console.log(solveQuadEquation(1, 7, 12)); 
+console.log(solveQuadEquation(1, 0, -4)); 
+console.log(solveQuadEquation(1, -1, 0));
+
+
+// L2.03 Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+const printArray = arr => {
+    if (!Array.isArray(arr)) return `Parameter ${arr} must be an array!`;
+    for (const item of arr) {
+        console.log(item);
+    }
+}
+
+console.log([0,1,2,3,4]);
+
+// L2.04 Write a function name showDateTime which 
+//       shows time in this format: 08/01/2020 04:08 using the Date object.
+// showDateTime()
+// 08/01/2020 04:08
+const showDateTime = () => {
+    const today = new  Date();
+    let MM = today.getMonth() + 1 > 9 ? today.getMonth() + 1 : '0'.concat(today.getMonth() + 1);
+    let DD = today.getDate() > 9 ? today.getDate() : '0'.concat(today.getDate());
+    let HH = today.getHours() > 9 ? today.getHours() : '0'.concat(today.getHours());
+    let mm = today.getMinutes() > 9 ? today.getMinutes() : '0'.concat(today.getMinutes());
+    let ss = today.getSeconds() > 9 ? today.getSeconds() : '0'.concat(today.getSeconds());
+    console.log(`${DD}/${MM}/${today.getFullYear()} ${HH}:${mm}`);
+}
+showDateTime();
+
+// Declare a function name swapValues. This function swaps value of x to y.
+
+// swapValues(3, 4) // x => 4, y=>3
+// swapValues(4, 5) // x = 5, y = 4
+// Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+
+// console.log(reverseArray([1, 2, 3, 4, 5]))
+// //[5, 4, 3, 2, 1]
+// console.log(reverseArray(['A', 'B', 'C']))
+// //['C', 'B', 'A']
+// Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+// Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+// Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+
+// Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
+
+// Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that - range.
+
+// Declare a function name sumOfEven. It takes a number parameter and it adds all the even numbers in that - range.
+
+// Declare a function name evensAndOdds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+// evensAndOdds(100);
+// The number of odds are 50.
+// The number of evens are 51.
+// Write a function which takes any number of arguments and return the sum of the arguments
+
+// sum(1, 2, 3) // -> 6
+// sum(1, 2, 3, 4) // -> 10
+// Writ a function which generates a randomUserIp.
+
+// Write a function which generates a randomMacAddress
+
+// Declare a function name randomHexaNumberGenerator. When this function is called it generates a random hexadecimal number. The function return the hexadecimal number.
+
+// console.log(randomHexaNumberGenerator());
+// '#ee33df'
+// Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+
+// console.log(userIdGenerator());
+// 41XTDbE
