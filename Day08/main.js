30993052415c6704800367a2c3fdf7120e29a18d
Tiziano Vicentini <tizvic@mc036.local>
2020-01-18 16:47:06 +0100

    Level 3 Ex 1

--- Day08/main.js
+++ Day08/main.js
@@ -116,10 +116,132 @@ for (const name of userNames) {
     }
 }
 
-// Set your name in the users object without modifying the original users object
+// L2.04 Set your name in the users object without modifying the original users object
+let myUsers = Object.assign({}, users);
+myUsers.TizVic = {
+  email: 'tizvic@me.com',
+  skills: ['HTML', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node', 'C#', 'Java', 'jQuery', 'MySQL', 'PostgreSQL', 'MSSQL'],
+  age: 50,
+  isLoggedIn: false,
+  points: 80
 
-// Get all keys or properties of users object
+}
+console.log(myUsers);
 
-// Get all the values of users object
+// L2.05 Get all keys or properties of users object
+let usersKeys = Object.keys(users);
+console.log(JSON.stringify(usersKeys));
 
-// Use the countries object to print a country name, capital, populations and languages.
+// L2.06 Get all the values of users object
+let usersValues = Object.values(users);
+console.log(usersValues);
+
+// L2.07 Use the countries object to print a country name, capital, populations and languages.
+for (let country of countries) {
+    console.log(`${country.name}\t${country.capital}\t${country.population}\t${country.languages}`);
+}
+
+/**
+ * Exercises: Level 3
+ */
+// L3.01 Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and 
+//       it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods.
+//       Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
+let personAccount = {
+  firstName:'',
+  lastName:'',
+  incomes:[],
+  expenses:[],
+  totalIncome: function() {},
+  totalExpense: function() {},
+  accountInfo: function() {},
+  addIncome: (income) => {this.incomes.push(income);},
+  addExpense: (expense) => {this.expenses.push(expense);},
+  accountBalance: () => {}
+}
+console.log(personAccount.addExpense([10, 'pizzas']));
+
+// **** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
+const users1 = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+];
+
+const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+];
+// L3.02 Imagine you are getting the above users collection from a MongoDB database. 
+//       a. Create a function called signUp which allows user to add to the collection.
+//          If user exists, inform the user that he has already an account. 
+//       b. Create a function called signIn which allows user to sign in to the application 
+
+// L3.03 The products array has three elements and each of them has six properties. 
+//       a. Create a function called rateProduct which rates the product 
+//       b. Create a function called averageRating which calculate the average rating of a product
+
+// L3.04 Create a function called likeProduct. This function will helps to like to the product if it is not liked 
+//       and remove like if it was liked.
