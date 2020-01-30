a42729295d50317b3462531bb91320e48b3a8b4e
Tiziano Vicentini <tiziano.vicentini@tiscali.it>
2020-01-30 22:03:23 +0100

    Level 3 to finish

--- Day15/main.js
+++ Day15/main.js
@@ -66,3 +66,18 @@ class NewCat extends Animal {
 let ermes = new NewCat('Ermes', 3, 'dark grey', 4, 'maooo');
 console.table(ermes);
 
+/**
+ * Level 3
+ */
+// L3.01 Let's try to develop a program which calculate measure of central tendency of a sample
+//       (mean, median, mode) and measure of variability(range, variance, standard deviation).
+//       In addition to those measures find the min, max, count, percentile, and frequency distribution
+//       of the sample. You can create a class called Statistics and create all the functions which do
+//       statistical calculations as method for the Statistics class.
+// TODO
+
+// L3.02 Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties 
+//       and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods.
+//       Incomes is a set of incomes and its description and expenses is also a set of expenses and its
+//       description.
+// TODO
